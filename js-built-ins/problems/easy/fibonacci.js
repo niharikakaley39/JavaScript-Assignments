42f987a5025e25994b9b2bdecc09ba670974cb43
niharikakaley39 <104619935+niharikakaley39@users.noreply.github.com>
2026-02-13 19:11:35 +0530

    done

--- js-built-ins/problems/easy/fibonacci.js
+++ js-built-ins/problems/easy/fibonacci.js
@@ -39,12 +39,20 @@
 
 // Iterative Fibonacci function
 function fibonacci(n) {
-  // Your code here
+  if( n <= 0) return [];
+  if(n === 1) return [0];
+  const seq =[0 , 1];
+  for(let i = 2; i < n; i++){
+    seq.push(seq[i - 1] + seq[i - 2]);
+  }
+  return seq;
 }
 
 // Recursive Fibonacci function
 function fibonacciRecursive(n) {
-  // Your code here
+  if( n <= 0) return 0;
+  if( n <= 1) return 1;
+   return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
 }
 
 module.exports = { fibonacci, fibonacciRecursive };
