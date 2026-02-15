05441ed93d92c528bd2fe6eaec8465180d7547fa
niharikakaley39 <104619935+niharikakaley39@users.noreply.github.com>
2026-02-15 12:59:39 +0530

    done non-repeat.js

--- js-built-ins/problems/medium/non-repeat.js
+++ js-built-ins/problems/medium/non-repeat.js
@@ -22,5 +22,15 @@
 */
 function nonrepeat(str) {
   // Your code here
+  const count = {};
+  for(let char of str){
+     count[char] = (count[char] || 0 + 1);
+  }
+  for(let char of str){
+    if(count[char] === 1){
+      return char;
+    }
+  }
+  return null;
 }
 module.exports = nonrepeat;
