8c52106710d3bd9a8b4215ee0ddb7535191a09be
niharikakaley39 <104619935+niharikakaley39@users.noreply.github.com>
2026-02-15 22:33:43 +0530

    done uniqueElements.js

--- js-built-ins/problems/hard/uniqueElements.js
+++ js-built-ins/problems/hard/uniqueElements.js
@@ -20,6 +20,16 @@
 
 function getUniqueElements(arr) {
   // Your code here
+  const seen = new Set();
+  const result =[];
+  for(const item of arr){
+    if(!seen.has(item)){
+       seen.add(item);
+       result.push(item);
+    }
+  }
+   return result;
+}
 }
 
 module.exports = getUniqueElements;
