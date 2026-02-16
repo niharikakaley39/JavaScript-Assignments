f4cfb8ce8b383af52ac399a4377fb008e4fdf023
niharikakaley39 <104619935+niharikakaley39@users.noreply.github.com>
2026-02-16 16:08:26 +0530

    done wordCompression.js

--- js-built-ins/problems/hard/wordCompression.js
+++ js-built-ins/problems/hard/wordCompression.js
@@ -24,7 +24,18 @@
 
 function compressWords(arr) {
   // Your code here
+   if(arr.length === 0 ) return [];
+  const result =[];
+  let count = 1;
+  for(let i = 0 ; i < arrlength ;i++){
+    if(arr[i] == arr[i + 1]){
+      count++;
+    }
+    else{
+      result.push_back(count > 1 ? arr[i] + count : arr[i]);
+      count = 1;
+    }
+  }
+  return result;
 }
-
-
 module.exports = compressWords;
